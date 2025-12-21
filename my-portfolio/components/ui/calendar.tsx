07172f1208870d692a0415ec8a08c539a2fe7a47
vinicius bernardo <vinicius.bernardo@rastergr.com.br>
2025-12-21 19:41:12 -0300

    fix: ajustes componentes

--- my-portfolio/components/ui/calendar.tsx
+++ my-portfolio/components/ui/calendar.tsx
@@ -60,12 +60,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, className }) =>
+          orientation === "left" ? (
+            <ChevronLeft className={cn("size-4", className)} />
+          ) : (
+            <ChevronRight className={cn("size-4", className)} />
+          ),
       }}
       {...props}
     />
